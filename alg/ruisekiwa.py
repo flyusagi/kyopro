70b1208dee8512e6c442a8957df38c8f6f314b3a
Kazuki Tanaka <tanaka.kazuki.18@shizuoka.ac.jp>
2021-07-16 10:29:25 +0900

    ruisekiwa zisso

---
+++ alg/ruisekiwa.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = []
+
+for i in range(n):
+  a.append(int(input()))
+
+# 累積和配列s
+s = [0]
+for i in range(n):
+  s.append(s[i] + a[i])
+
+#1から3までの和を求める
+ans = s[4]-s[1]
+print(ans)
