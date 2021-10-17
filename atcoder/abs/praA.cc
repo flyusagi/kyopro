7df63bf68a649b213339df6bb6e3a4321bd39e84
Kazuki Tanaka <tanaka.kazuki.18@shizuoka.ac.jp>
2021-10-18 04:21:26 +0900

    absを3問やった

---
+++ atcoder/abs/praA.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c;
+  string s;
+  cin >> a >> b >> c >> s;
+  int abc = a + b + c;
+  cout << abc << " " << s;
+
+  return 0;
+}
