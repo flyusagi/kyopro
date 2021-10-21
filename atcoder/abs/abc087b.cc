22f6655c99737b27aea15c66bdbba8d18a3287ab
Kazuki Tanaka <tanaka.kazuki.18@shizuoka.ac.jp>
2021-10-21 20:52:38 +0900

    abc087b toita

---
+++ atcoder/abs/abc087b.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int count = 0;
+  int A,B,C,X;
+  cin >> A >> B >> C >> X;
+  for(int i = 0;i <= A;i++){
+    for(int j = 0;j <= B;j++){
+      for(int k = 0;k <= C;k++){
+        if(500*i + 100*j + 50*k == X){
+          count++;
+        }
+      }
+    }
+  }
+  cout << count;
+}
